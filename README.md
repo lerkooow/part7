ef628baba67a05ec01099634e056dd1cfc9b5abd
lerkooow <lerkoooow@gmail.com>
2023-09-25 19:07:53 +0700

    7.1: routed anecdotes, step1

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# React + Vite
+# React router, custom hooks, styling app with CSS and webpack  
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## React Router  
 
-Currently, two official plugins are available:
+25.09.2023  
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+7.1: routed anecdotes, step1  
+
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
-  )
+const padding = {
+      paddingRight: 5
 }
 
 const AnecdoteList = ({ anecdotes }) => (
@@ -123,14 +118,26 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+   <div>
+     <Router>
+     <h1>Software anecdotes</h1>
+      <div>
+        <Link style={padding} to="/">anecdotes</Link>
+        <Link style={padding} to="/create">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <div>
+        <Footer />
+      </div>
+    </Router>
+   </div>
   )
 }
 
